e54052e6da6bb89e4b19c1f36600db113491b17d
George <GZheng8918@gmail.com>
2023-04-14 22:09:55 -0700

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // Add event listener to form submit
+  document.getElementById('create-task-form').addEventListener('submit',(e) => {
+    e.preventDefault();
+    handleList(e.target['new-task-description'].value);
+    e.target.reset(); 
+  });
 });
+
+function handleList(todo) {
+  let li = document.createElement('li');
+  li.textContent = todo;
+  document.getElementById('tasks').appendChild(li); 
+}
+
+
